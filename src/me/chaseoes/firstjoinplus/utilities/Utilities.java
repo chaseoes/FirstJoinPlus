fdf021aeef73c68935ebee1c4fc29299225fe77f
chaseoes <chase.oes@gmail.com>
2013-03-10 17:49:40 -0700

    Added ability to delay giving items and written books (fixes #8).

--- src/me/chaseoes/firstjoinplus/utilities/Utilities.java
+++ src/me/chaseoes/firstjoinplus/utilities/Utilities.java
@@ -57,73 +57,83 @@ public class Utilities {
         return new Location(plugin.getServer().getWorld(plugin.getConfig().getString("spawn.world")), x + 0.5, y, z + 0.5, yaw, pitch);
     }
 
-    public void giveFirstJoinItems(Player player) {
-        for (String itemStr : plugin.getConfig().getStringList("on-first-join.give-items.items")) {
-            ItemStack i;
-            String[] itemValues = itemStr.split("\\:");
+    public void giveFirstJoinItems(final Player player) {
+        plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+            @Override
+            public void run() {
+                for (String itemStr : plugin.getConfig().getStringList("on-first-join.give-items.items")) {
+                    ItemStack i;
+                    String[] itemValues = itemStr.split("\\:");
 
-            if (isNumber(itemValues[0])) {
-                i = new ItemStack(Material.getMaterial(Integer.parseInt(itemValues[0])));
-            } else {
-                i = new ItemStack(Material.getMaterial(itemValues[0].toUpperCase()));
+                    if (isNumber(itemValues[0])) {
+                        i = new ItemStack(Material.getMaterial(Integer.parseInt(itemValues[0])));
+                    } else {
+                        i = new ItemStack(Material.getMaterial(itemValues[0].toUpperCase()));
+                    }
+
+                    if (itemValues.length > 1) {
+                        i.setAmount(Integer.parseInt(itemValues[1]));
+                    }
+
+                    if (itemValues.length > 2) {
+                        i = new ItemStack(i.getType(), i.getAmount(), (short) Integer.parseInt(itemValues[2]));
+                    }
+
+                    player.getInventory().addItem(i);
+                }
             }
-
-            if (itemValues.length > 1) {
-                i.setAmount(Integer.parseInt(itemValues[1]));
-            }
-
-            if (itemValues.length > 2) {
-                i = new ItemStack(i.getType(), i.getAmount(), (short) Integer.parseInt(itemValues[2]));
-            }
-
-            player.getInventory().addItem(i);
-        }
+        }, plugin.getConfig().getLong("on-first-join.give-items.delay"));
     }
 
-    public void giveWrittenBooks(Player player) {
-        for (String file : plugin.getConfig().getStringList("on-first-join.give-written-books.book-files")) {
-            ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
-            BookMeta bm = (BookMeta) book.getItemMeta();
-            File f = new File(plugin.getDataFolder() + "/" + file);
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(f));
-                StringBuilder sb = new StringBuilder();
-                String line = br.readLine();
-                int i = 0;
+    public void giveWrittenBooks(final Player player) {
+        plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+            @Override
+            public void run() {
+                for (String file : plugin.getConfig().getStringList("on-first-join.give-written-books.book-files")) {
+                    ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+                    BookMeta bm = (BookMeta) book.getItemMeta();
+                    File f = new File(plugin.getDataFolder() + "/" + file);
+                    try {
+                        BufferedReader br = new BufferedReader(new FileReader(f));
+                        StringBuilder sb = new StringBuilder();
+                        String line = br.readLine();
+                        int i = 0;
 
-                while (line != null) {
-                    i++;
+                        while (line != null) {
+                            i++;
 
-                    if (i != 1 && i != 2) {
-                        if (line.equalsIgnoreCase("/newpage") || line.equalsIgnoreCase("/np")) {
-                            bm.addPage(sb.toString());
-                            sb = new StringBuilder();
-                        } else {
-                            sb.append(Utilities.getUtilities().colorize(line));
-                            sb.append("\n");
+                            if (i != 1 && i != 2) {
+                                if (line.equalsIgnoreCase("/newpage") || line.equalsIgnoreCase("/np")) {
+                                    bm.addPage(sb.toString());
+                                    sb = new StringBuilder();
+                                } else {
+                                    sb.append(Utilities.getUtilities().colorize(line));
+                                    sb.append("\n");
+                                }
+                            } else {
+                                if (i == 1) {
+                                    bm.setTitle(Utilities.getUtilities().colorize(line));
+                                }
+                                if (i == 2) {
+                                    bm.setAuthor(Utilities.getUtilities().colorize(line));
+                                }
+                            }
+                            line = br.readLine();
                         }
-                    } else {
-                        if (i == 1) {
-                            bm.setTitle(Utilities.getUtilities().colorize(line));
-                        }
-                        if (i == 2) {
-                            bm.setAuthor(Utilities.getUtilities().colorize(line));
-                        }
+
+                        br.close();
+                        bm.addPage(sb.toString());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        plugin.getLogger().log(Level.WARNING, "Error encountered while trying to give a new player a written book! (File " + file + ")");
+                        plugin.getLogger().log(Level.WARNING, "Please check that the file exists and is readable.");
                     }
-                    line = br.readLine();
+
+                    book.setItemMeta(bm);
+                    player.getInventory().addItem(book);
                 }
-
-                br.close();
-                bm.addPage(sb.toString());
-            } catch (Exception e) {
-                e.printStackTrace();
-                plugin.getLogger().log(Level.WARNING, "Error encountered while trying to give a new player a written book! (File " + file + ")");
-                plugin.getLogger().log(Level.WARNING, "Please check that the file exists and is readable.");
             }
-
-            book.setItemMeta(bm);
-            player.getInventory().addItem(book);
-        }
+        }, plugin.getConfig().getLong("on-first-join.give-written-books.delay"));
     }
 
     public void playSmoke(Location loc) {
